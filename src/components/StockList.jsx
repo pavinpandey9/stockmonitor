337dd71cbbd6f788ddcc488c14097bef42eb5360
Pavin <81810380+pavinpandey9@users.noreply.github.com>
2024-10-22 19:17:31 +0530

    Update StockList.jsx

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import FilterStock from "./FilterStock";
 import SortStock from "./SortStock";
 import StockItem from "./StockItem";
